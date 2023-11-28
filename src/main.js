8483b70f7c71ebfffcce8632967ad881fe712c54
dwigunardi <dwigunardi@gmail.com>
2023-11-29 01:23:54 +0700

    del: pinia and add: useVue

--- src/main.js
+++ src/main.js
@@ -8,10 +8,6 @@ import router from './router';
 // make sure to also import the coresponding css
 import "@mdi/font/css/materialdesignicons.css"; // Ensure you are using css-loader
 import "@fortawesome/fontawesome-free/css/all.css"; // Ensure your
-import { createPinia } from 'pinia' 
 import { vuetify } from './vuetify';
 
-
-const pinia = createPinia()
-
-createApp(App).use(pinia).use(vuetify).use(router).mount('#app')
+createApp(App).use(vuetify).use(router).mount('#app')
